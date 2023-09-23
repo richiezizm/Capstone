2d00ac7a35e258df00aa549e18a940a6d0f69a2b
Rich <rsenquis@gmail.com>
2023-09-22 21:31:44 -0400

    Added React folder and polished flex boxes a bit

---
+++ cap.js
@@ -0,0 +1,9 @@
+const menuItem = {
+    "name": this.name,
+    "dishType": this.dishType,
+    "flavor": this.flavor,
+    "price": this.price    
+};
+
+let strawberry = new menuItem("strawberry", "dessert", "strawberry", "5");
+console.log(strawberry);
